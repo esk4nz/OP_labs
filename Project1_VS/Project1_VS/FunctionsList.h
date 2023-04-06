b4d83cee8f3ac13d576299b1b3ef6f3bde3e7920
esk4nz <grinko27052005@gmail.com>
2023-04-06 21:59:23 +0300

    first labaratory

---
+++ Project1_VS/Project1_VS/FunctionsList.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+void names();
+void inputFile(string, string, string);
+void deleteLastSymbol(string, string, string);
+void addNewText(string, string, string);
+void deleteCommasAndDots(string, string, string);
+void moreThanN(string, string, string arr[], int size);
+void makeArrayOfWords(string, string);
+void textOut(string, string);
---
+++ Project1_VS/Project1_VS/FunctionsListC.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cstdlib>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+void namesC();
+void inputFileC(char*, char*, char*);
+void deleteLastSymbolC(char*, char*, char*);
+void addNewTextC(char*, char*, char*);
+void deleteCommasAndDotsC(char*, char*, char*);
+void moreThanNC(char*, char*, char** arr, int size);
+void makeArrayOfWordsC(char*, char*);
+void textOutC(char*, char*);
